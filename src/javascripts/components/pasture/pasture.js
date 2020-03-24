ce51157ab471fed58d6a8f86022c112ccd7f7d55
delindell <daviselindell@gmail.com>
2020-03-23 19:23:23 -0500

    printing cows

--- src/javascripts/components/pasture/pasture.js
+++ src/javascripts/components/pasture/pasture.js
@@ -1,9 +1,21 @@
-
+import utils from '../../helpers/utils';
 import cowData from '../../helpers/data/cowData';
+import cowComponent from '../cow/cow';
 
 const buildCows = () => {
   cowData.getCows()
-    .then((response) => console.error('get cows worked!!!', response.data))
+    .then((cows) => {
+      let domString = '';
+      domString += '<h2 class="text-center">Pasture</h2>';
+      domString += '<div class="d-flex flex-wrap">';
+      cows.forEach((cow) => {
+        domString += cowComponent.cowMaker(cow);
+      });
+      domString += '</div>';
+      utils.printToDom('pasture', domString);
+      // domString +=
+      // domString +=
+    })
     .catch((err) => console.error('get cows broke', err));
 };
 
